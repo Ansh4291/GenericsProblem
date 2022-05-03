efb14fcd0106711d160acc24ad456bc88e9d89b2
AnshulRathore <anshulrathore1995@gmail.com>
2022-05-03 12:42:08 +0530

    Added: Maximum of Three Strings are added

--- src/com/bridgelabz/FindMaxValueByGenerics.java
+++ src/com/bridgelabz/FindMaxValueByGenerics.java
@@ -21,8 +21,18 @@ public class FindMaxValueByGenerics {
         // Print the max number
         System.out.println("Maximum number out of 3 numbers are "+a+ "," +b+ "," +c+ " is :- " + max);
     }
+    // Given 3 strings find the maximum
+    public static void findMaxValue(String s1 , String s2 , String s3){
+        String max = s1;
+        if (s2.compareTo(max) > 0)
+            max = s2;
+        if (s3.compareTo(max) > 0)
+            max = s3;
+        System.out.println("Maximum number out of 3 Strings are "+s1+ "," +s2+ "," +s3+ " is :- " + max);
+    }
     public static void main(String[] args) {
         FindMaxValueByGenerics.findMaxValue(10, 20 , 30);
         FindMaxValueByGenerics.findMaxValue(3.5f, 10.50f, 15.60f);
+        FindMaxValueByGenerics.findMaxValue("Smith" , "Jhon" , " rock");
     }
 }
