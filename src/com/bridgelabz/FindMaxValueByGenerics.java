542e602a7b20468db190866c814aeb1e651b149d
AnshulRathore <anshulrathore1995@gmail.com>
2022-05-03 12:17:31 +0530

    Added:Find the max of three integer

--- src/com/bridgelabz/FindMaxValueByGenerics.java
+++ src/com/bridgelabz/FindMaxValueByGenerics.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
 public class FindMaxValueByGenerics {
+    public static void findMaxValue( Integer a, Integer b , Integer c){
+        Integer max = a;
+        if (b.compareTo(max) > 0)
+            max = b;
+        if (c.compareTo(max) > 0)
+            max = c;
+        // Print the max number
+        System.out.println("Maximum number out of 3 numbers are "+a+ "," +b+ "," +c+ " is :- " +max);
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to the Maximum Problem using Generics :-) ");
+        FindMaxValueByGenerics.findMaxValue(10, 20 , 30);
     }
 }
